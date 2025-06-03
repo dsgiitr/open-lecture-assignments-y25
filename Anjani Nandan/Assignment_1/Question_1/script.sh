45ea1f6ff15303ccc69c39a26e8af67b405e8647
anjaninandan001 <145668933+anjaninandan001@users.noreply.github.com>
2025-06-03 12:05:12 +0530

    Update script.sh

--- Anjani Nandan/Assignment_1/Question_1/script.sh
+++ Anjani Nandan/Assignment_1/Question_1/script.sh
@@ -1,1 +1,21 @@
+#Created a Bash Script for Log Cleanup
+#!/bin/bash
+
+TARGET_DIR="/DSG"
+
+find "$TARGET_DIR" -type f -name "*.log" -mtime +7 -print -delete
+
+echo "Deleted .log files older than 7 days in $TARGET_DIR"
+
+
+# Made the Script Executable
+chmod +x /root/clean_logs.sh
+
+#Tested the Script
+bash /root/clean_logs.sh
+
+#Set Up a Cron Job for Automation
+crontab -e
+0 8 * * * /root/clean_logs.sh >> /root/clean_logs.log 2>&1
+sudo service cron start
 
